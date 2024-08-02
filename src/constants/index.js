3c6092a3e0d95a30fead59ba7ec13df278c2ea7d
karanchauke <karanchauke7083@outlook.com>
2024-08-02 09:05:18 +0530

    PORTFOLIO PROJECTS

---
+++ src/constants/index.js
@@ -0,0 +1,347 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    carrent1 ,
+    twitter ,
+    Redux ,
+    dsa ,
+    mern ,
+    harkirat 
+    
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Full Stack Developer",
+      icon: web,
+    },
+    {
+      title: "React Developer",
+      icon: mobile,
+    },
+    {
+      title: "Frontend Developer",
+      icon: backend,
+    },
+    {
+      title: "Backend Developer",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Starbucks",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "React Native Developer",
+      company_name: "Tesla",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "Meta",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Study-Notion",
+      description:
+        "Web-based education platform that allows users to study online, dashboard, and manage , studynotion from various course providers, providing a buy student course with integrate a payment in Razorpay solution for Education Platform anytime , anywhere .",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "TailwindCSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent1,
+      source_code_link: "https://study-notion-a3f3c6.netlify.app/",
+    },
+    {
+      name: "X Application",
+      description:
+        "Web Application social media creating few functional web application that core features of X, including user authentication using secure hashing algorithm for password . posting , tweet creation, likes, retweets , follow , unfollow , and real-time updates .",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Reastapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Redux",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: twitter,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "ShopCart",
+      description:
+        "Create a React application for a project that includes a shopping cart. Implement the functionality to add and remove product information using Redux Toolkit .",
+      tags: [
+        {
+          name: "Redux Toolkit",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Javascript",
+          color: "green-text-gradient",
+        },
+        {
+          name: "TailwindCSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: Redux,
+      source_code_link: "https://o-n-l-i-n-e-shop-cart-1182vy0le-karan-chaukes-projects.vercel.app/",
+    },
+  ];
+
+  const certificates = [
+    {
+      name: "DSA - C O D E H E L P",
+      description:
+
+        "LOVE BABBAR - Founder - Code-Help, Ex-Amazon, Ex-Microsoft",
+
+      tags: [
+        {
+          name: "Problem-Solving Skills",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Algorithmic Thinking",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Competitive Programming",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: dsa,
+      source_code_link: "https://www.codehelp.in/",
+    },
+
+    {
+      name: "MERN - C O D E H E L P",
+      description:
+
+        "LOVE BABBAR - Founder - Code-Help, Ex-Amazon, Ex-Microsoft",
+
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Mongodb || Node",
+          color: "green-text-gradient",
+        },
+        {
+          name: "TailwindCSS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: mern,
+      source_code_link: "https://www.codehelp.in/",
+    },
+    {
+      name: "MERN ( Advance ) - 100x Devs ",
+      description:
+
+        "Harkirat Singh - Founder - 100XDev Google's Summer Code X2 and worked at FAANG, Finance and Startups ",
+
+      tags: [
+        {
+          name: "Typescript",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Docker || Kubernetes",
+          color: "green-text-gradient",
+        },
+        {
+          name: "AWS",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: harkirat,
+      source_code_link: "https://harkirat.classx.co.in/",
+    },
+  ];
+
+
+  
+  export { services, technologies, experiences, testimonials, projects , certificates};
